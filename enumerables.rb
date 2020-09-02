f133d24e50445890a477fd58058a3963d24007ec
bmuhamood <bbosa2009@gmail.com>
2020-09-02 21:53:44 +0400

    added loop in the my_none? method

--- enumerables.rb
+++ enumerables.rb
@@ -78,33 +78,6 @@ def my_none?
   end
 end
 
-
-
-
-
-
-
-
-def any
-  return to_enum unless block_given?
-
-    mynum1 = [10, 19, 18]    
-    myres1 = mynum1.any? { |num| num>13}
-    puts res1
-    res2 = mynum1.any? { |num| num>=20}
-    puts res2  
-end
-
-def my_none?
-  return to_enum unless block_given?
-
-  my_each do |item|
-    return false if block_given? && yield(item) || !block_given? && item
-  end
-    true
-  end                        
-end 
-
 def my_count
   return to_enum unless block_given?
 
