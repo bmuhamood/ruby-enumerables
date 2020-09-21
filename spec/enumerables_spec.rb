a113e4c10586d2f14d90f628f074dcde994ece1b
bmuhamood <bbosa2009@gmail.com>
2020-09-22 02:15:16 +0400

    added Gem files

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,5 +1,7 @@
-# frozen_string_literal: false
-
+# rubocop:disable Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/PerceivedComplexity
+# rubocop:disable Metrics/AbcSize
+# rubocop:disable Metrics/MethodLength
 require_relative '../lib/enumerables'
 describe '#my_each' do
   it 'return enumerator if block is not given' do
@@ -203,3 +205,8 @@ public 'my_inject'
 public 'my_inject'
 public 'multiply_els'
 public 'my_select'
+
+# rubocop:enable Metrics/AbcSize
+# rubocop:enable Metrics/MethodLength
+# rubocop:enable Metrics/CyclomaticComplexity
+# rubocop:enable Metrics/PerceivedComplexity
