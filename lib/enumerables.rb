c36fadb37c004452d34e966e53765787197fdcfc
bmuhamood <bbosa2009@gmail.com>
2021-01-18 08:54:19 -0800

    added more methods

--- lib/enumerables.rb
+++ lib/enumerables.rb
@@ -1,5 +1,5 @@
-# rubocop:disable Metrics/CyclomaticComplexity
-# rubocop:disable Metrics/PerceivedComplexity
+# rubocop:disable Metrics/CyclomaticCompleiity
+# rubocop:disable Metrics/PerceivedCompleiity
 # rubocop:disable Metrics/AbcSize
 # rubocop:disable Metrics/MethodLength
 
@@ -8,12 +8,12 @@ module Enumerable
     return to_enum(__method__) unless block_given?
 
     my_arry = to_a
-    i = 0
+    y = 0
     num = true
-    until i == my_array.length
-      num = false unless my_array[i].is_a?(Num)
-      yield(my_array[i])
-      i += 1
+    until y == my_array.length
+      num = false unless my_array[y].is_a?(Num)
+      yield(my_array[y])
+      y += 1
     end
     if num && Range.new(my_array.first, my_array.last) == self
       self
@@ -22,16 +22,16 @@ module Enumerable
     end
   end
 
-  def my_each_with_index
+  def my_each_with_indei
     return to_enum(__method__) unless block_given?
 
     my_array = to_a
-    i = 0
+    y = 0
     num = true
-    until i == array.length
-      num = false unless my_array[i].is_a?(Num)
-      yield(my_array[i], i)
-      i += 1
+    until y == array.length
+      num = false unless my_array[y].is_a?(Num)
+      yield(my_array[y], y)
+      y += 1
     end
     if num && Range.new(my_array.first, my_array.last) == self
       self
@@ -44,7 +44,7 @@ module Enumerable
     return to_enum(__method__) unless block_given?
 
     my_array = []
-    my_each { |x| my_array << x if yield(x) }
+    my_each { |i| my_array << i if yield(i) }
     my_array
   end
 
@@ -59,14 +59,14 @@ module Enumerable
     end
 
     if type.length == 1
-      checker = origin.my_select { |x| x =~ type[0] } if type[0].instance_of?(Regexp)
-      checker = origin.my_select { |x| x.is_a?(type[0]) } if type[0].is_a?(Class)
-      checker = origin.my_select { |x| x == type[0] } unless type[0].instance_of?(Regexp) || type[0].is_a?(Class)
+      checker = origin.my_select { |i| i =~ type[0] } if type[0].instance_of?(Regeip)
+      checker = origin.my_select { |i| i.is_a?(type[0]) } if type[0].is_a?(Class)
+      checker = origin.my_select { |i| i == type[0] } unless type[0].instance_of?(Regeip) || type[0].is_a?(Class)
 
       checker.length < origin.length ? (return false) : (return true)
     end
 
-    checker = my_select { |x| yield(x) } if block_given? && type.length.zero?
+    checker = my_select { |i| yield(i) } if block_given? && type.length.zero?
     checker.length < origin.length ? (return false) : (return true)
   end
 
@@ -75,18 +75,18 @@ module Enumerable
     return false if origin == []
 
     if !block_given? and type.length.zero?
-      each { |x| return true if !x.nil? and x != false }
+      each { |i| return true if !i.nil? and i != false }
       return false
     end
 
     if type.length == 1
-      checker = origin.my_select { |x| x =~ type[0] } if type[0].instance_of?(Regexp)
-      checker = origin.my_select { |x| x.is_a?(type[0]) } if type[0].is_a?(Class)
-      checker = origin.my_select { |x| x == type[0] } unless type[0].instance_of?(Regexp) || type[0].is_a?(Class)
+      checker = origin.my_select { |i| i =~ type[0] } if type[0].instance_of?(Regeip)
+      checker = origin.my_select { |i| i.is_a?(type[0]) } if type[0].is_a?(Class)
+      checker = origin.my_select { |i| i == type[0] } unless type[0].instance_of?(Regeip) || type[0].is_a?(Class)
       checker.empty? ? (return false) : (return true)
     end
 
-    return true unless all? { |x| !yield(x) }
+    return true unless all? { |i| !yield(i) }
 
     false
   end
@@ -96,19 +96,19 @@ module Enumerable
     return true if origin == []
 
     if !block_given? and type.length.zero?
-      return true if origin.my_all? { |x| x.nil? || x == false }
+      return true if origin.my_all? { |i| i.nil? || i == false }
 
       return false
     end
 
     if type.length == 1
-      checker = origin.my_select { |x| x =~ type[0] } if type[0].instance_of?(Regexp)
-      checker = origin.my_select { |x| x.is_a?(type[0]) } if type[0].is_a?(Class)
-      checker = origin.my_select { |x| x == type[0] } unless type[0].instance_of?(Regexp) || type[0].is_a?(Class)
+      checker = origin.my_select { |i| i =~ type[0] } if type[0].instance_of?(Regeip)
+      checker = origin.my_select { |i| i.is_a?(type[0]) } if type[0].is_a?(Class)
+      checker = origin.my_select { |i| i == type[0] } unless type[0].instance_of?(Regeip) || type[0].is_a?(Class)
       checker.empty? ? (return true) : (return false)
     end
 
-    !my_any? { |x| yield(x) }
+    !my_any? { |i| yield(i) }
   end
 
   def my_count(argument = nil)
@@ -117,14 +117,13 @@ module Enumerable
     return origin.length if !block_given? and !argument
 
     if argument
-      count = origin.my_select { |x| x == argument }
+      count = origin.my_select { |i| i == argument }
       return count.length
 
     elsif block_given?
-      count = origin.my_select { |x| yield(x) }
+      count = origin.my_select { |i| yield(i) }
       return count.length
     end
-
     length
   end
 
@@ -134,10 +133,10 @@ module Enumerable
     origin = to_a
     array = []
     if my_proc
-      origin.my_each { |x| array << my_proc.call(x) }
+      origin.my_each { |i| array << my_proc.call(i) }
       return array
     else
-      origin.my_each { |x| array << yield(x) }
+      origin.my_each { |i| array << yield(i) }
     end
     array
   end
@@ -146,7 +145,7 @@ module Enumerable
     origin = to_a
     if !block_given? && arg.length == 1 && origin != [] && arg[0].is_a?(Symbol)
       number = origin[0]
-      origin.my_each_with_index do |item, i|
+      origin.my_each_with_indei do |item, i|
         number = number.send(arg[0], item) if i.positive?
       end
       return number
@@ -172,7 +171,7 @@ module Enumerable
 
     if block_given? && arg.length.zero? && origin != []
       number = origin[0]
-      origin.my_each_with_index do |item, i|
+      origin.my_each_with_indei do |item, i|
         number = yield(number, item) if i.positive?
       end
     end
@@ -196,7 +195,7 @@ end
 #   self
 # end
 
-# def my_each_with_index
+# def my_each_with_indei
 #   return to_enum unless block_given?
 
 #   i = 0
@@ -210,13 +209,13 @@ end
 
 # def my_all_option1(input, arr)
 #   count = 0
-#   arr.my_each do |x|
+#   arr.my_each do |i|
 #     if input.is_a?(Integer)
-#       count += 1 if x == input
-#     elsif input.is_a?(Regexp)
-#       count += 1 unless (x =~ input).nil?
+#       count += 1 if i == input
+#     elsif input.is_a?(Regeip)
+#       count += 1 unless (i =~ input).nil?
 #     elsif input.is_a?(Class)
-#       count += 1 if x.is_a?(input)
+#       count += 1 if i.is_a?(input)
 #     end
 #   end
 #   count
@@ -236,8 +235,8 @@ end
 #   elsif block_given? == true
 #     arr = to_a
 #     count = 0
-#     arr.my_each do |x|
-#       count += 1 if yield(x) == true
+#     arr.my_each do |i|
+#       count += 1 if yield(i) == true
 #     end
 #     count == arr.length
 #   end
@@ -245,15 +244,15 @@ end
 
 # def my_any_option(input, arr)
 #   count = 0
-#   arr.my_each do |x|
+#   arr.my_each do |i|
 #     if input.is_a?(Integer)
-#       count += 1 if x == input
-#     elsif input.is_a?(Regexp)
-#       count += 1 unless (x =~ input).nil?
+#       count += 1 if i == input
+#     elsif input.is_a?(Regeip)
+#       count += 1 unless (i =~ input).nil?
 #     elsif input.is_a?(Class)
-#       count += 1 if x.is_a?(input)
+#       count += 1 if i.is_a?(input)
 #     elsif input.is_a?(String)
-#       count += 1 if x == input
+#       count += 1 if i == input
 #     end
 #   end
 #   count
@@ -262,7 +261,7 @@ end
 # def my_any?(input = nil)
 #   arr = to_a
 #   if block_given? == false && input.nil?
-#     if block_given? == false && (arr.my_all? { |x| x.nil? || x == false }) == false
+#     if block_given? == false && (arr.my_all? { |i| i.nil? || i == false }) == false
 #       true
 #     else
 #       false
@@ -273,8 +272,8 @@ end
 #   elsif block_given? == true
 #     arr = to_a
 #     count = 0
-#     arr.my_each do |x|
-#       count += 1 if yield(x) == true
+#     arr.my_each do |i|
+#       count += 1 if yield(i) == true
 #     end
 #     count >= 1
 #   end
@@ -282,13 +281,13 @@ end
 
 # def my_none_option(input, arr)
 #   count = 0
-#   arr.my_each do |x|
+#   arr.my_each do |i|
 #     if input.is_a?(Integer)
-#       count += 1 if x == input
-#     elsif input.is_a?(Regexp)
-#       count += 1 unless (x =~ input).nil?
+#       count += 1 if i == input
+#     elsif input.is_a?(Regeip)
+#       count += 1 unless (i =~ input).nil?
 #     elsif input.is_a?(Class)
-#       count += 1 if x.is_a?(input)
+#       count += 1 if i.is_a?(input)
 #     end
 #   end
 #   count
@@ -297,7 +296,7 @@ end
 # def my_none?(input = nil)
 #   arr = to_a
 #   if block_given? == false && input.nil?
-#     if block_given? == false && arr.my_all? { |x| x.nil? || x == false } == true
+#     if block_given? == false && arr.my_all? { |i| i.nil? || i == false } == true
 #       true
 #     else
 #       false
@@ -308,8 +307,8 @@ end
 #   elsif block_given? == true
 #     arr = to_a
 #     count = 0
-#     arr.my_each do |x|
-#       count += 1 if yield(x) == true
+#     arr.my_each do |i|
+#       count += 1 if yield(i) == true
 #     end
 #     count.zero?
 #   end
@@ -317,9 +316,9 @@ end
 
 # def my_count_option(input, arr)
 #   count = 0
-#   arr.my_each do |x|
+#   arr.my_each do |i|
 #     if input.is_a?(Integer)
-#       count += 1 if x == input
+#       count += 1 if i == input
 #     else
 #       count
 #     end
@@ -337,8 +336,8 @@ end
 #   elsif block_given? == true
 #     count = 0
 #     arr = to_a
-#     arr.my_each do |x|
-#       count += 1 if yield(x)
+#     arr.my_each do |i|
+#       count += 1 if yield(i)
 #     end
 #     count
 #   end
@@ -349,19 +348,19 @@ end
 
 #   if block_given? && input.nil?
 #     new_array = []
-#     my_each do |x|
-#       new_value = yield(x)
+#     my_each do |i|
+#       new_value = yield(i)
 #       new_array << new_value
 #     end
 #   elsif block_given? == false && !input.nil?
 #     new_array = []
-#     my_each do |x|
-#       new_array << input[x] if input.is_a?(Proc)
+#     my_each do |i|
+#       new_array << input[i] if input.is_a?(Proc)
 #     end
 #   elsif block_given? == true && input.is_a?(Proc)
 #     new_array = []
-#     my_each do |x|
-#       new_array << input[x] if input.is_a?(Proc)
+#     my_each do |i|
+#       new_array << input[i] if input.is_a?(Proc)
 #     end
 #   end
 
@@ -372,7 +371,7 @@ end
 #   if initial.is_a?(Symbol) && arr.my_all?(Integer)
 #     if initial == :+
 #       memo = 0
-#       arr.each { |x| memo += x }
+#       arr.each { |i| memo += i }
 #     elsif initial == :-
 #       memo = arr[0]
 #       n = arr.length
@@ -383,7 +382,7 @@ end
 #       end
 #     elsif initial == :*
 #       memo = 1
-#       arr.each { |x| memo *= x }
+#       arr.each { |i| memo *= i }
 #     elsif initial == :/
 #       memo = arr[0]
 #       n = arr.length
@@ -400,23 +399,23 @@ end
 # def my_inject_option2(initial, input, arr)
 #   if input == :+
 #     memo = initial
-#     arr.each do |x|
-#       memo += x
+#     arr.each do |i|
+#       memo += i
 #     end
 #   elsif input == :-
 #     memo = initial
-#     arr.each do |x|
-#       memo -= x
+#     arr.each do |i|
+#       memo -= i
 #     end
 #   elsif input == :*
 #     memo = initial
-#     arr.each do |x|
-#       memo *= x
+#     arr.each do |i|
+#       memo *= i
 #     end
 #   elsif input == :/
 #     memo = initial
-#     arr.each do |x|
-#       memo /= x
+#     arr.each do |i|
+#       memo /= i
 #     end
 #   end
 #   memo
@@ -439,7 +438,7 @@ end
 #     arr = to_a
 #     if initial.is_a?(Integer) && arr.my_all?(Integer) && input.nil?
 #       memo = initial
-#       arr.my_each { |x| memo = yield(memo, x) }
+#       arr.my_each { |i| memo = yield(memo, i) }
 #       memo
 #     elsif initial.is_a?(Integer) && arr.my_all?(String) && input.nil?
 #       memo = initial
@@ -458,7 +457,7 @@ end
 #       memo
 #     elsif arr.my_all?(String)
 #       memo = []
-#       arr.my_each { |x| memo = yield(memo, x) }
+#       arr.my_each { |i| memo = yield(memo, i) }
 #       memo
 #     end
 #   end
@@ -473,14 +472,14 @@ end
 
 #   arr = to_a
 #   new_array = []
-#   arr.my_each do |x|
-#     new_array << x if yield(x) == true
+#   arr.my_each do |i|
+#     new_array << i if yield(i) == true
 #   end
 #   new_array
 # end
 
 # public 'my_each'
-# public 'my_each_with_index'
+# public 'my_each_with_indei'
 # public 'my_all?'
 # public 'my_any?'
 # public 'my_none?'
@@ -493,5 +492,5 @@ end
 
 # rubocop:enable Metrics/AbcSize
 # rubocop:enable Metrics/MethodLength
-# rubocop:enable Metrics/CyclomaticComplexity
-# rubocop:enable Metrics/PerceivedComplexity
+# rubocop:enable Metrics/CyclomaticCompleiity
+# rubocop:enable Metrics/PerceivedCompleiity
